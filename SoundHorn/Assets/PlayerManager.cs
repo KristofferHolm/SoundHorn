0746a32b934ace527371a8e958f8028f00405fbc
Kristoffer/Desktop <Kristoffer_lh@hotmail.com>
2018-02-06 20:01:19 +0100

    Standard Player Controller added

---
+++ SoundHorn/Assets/PlayerManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BaseClasses;
+
+namespace Managers
+{
+    public class PlayerManager : BaseSingleton<PlayerManager> {
+
+        public float MovementSpeed;
+        public float MouseSensitivity;
+        public bool LockMovement = false;
+        public bool InvertMouse = false;
+        //references
+
+        public GameObject Player;
+        // Use this for initialization
+        void Start() {
+
+        }
+
+        // Update is called once per frame
+        void Update() {
+        }
+    }
+}
---
+++ SoundHorn/Assets/SoundHorn/Scripts/BaseClasses/BaseVariables.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BaseClasses
+{
+    public class nVector : MonoBehaviour{
+
+        public static Vector3 up;
+        public static Vector3 one;
+        public static Vector3 zero;
+        private void Awake()
+        {
+            up = Vector3.up;
+            one = Vector3.one;
+            zero = Vector3.zero;
+        }
+    }
+}
